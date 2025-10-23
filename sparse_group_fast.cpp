7a9062272ec2784b1d0a2832ce8a1f8755aa2a58
Etienne Wijler <e.j.j.wijler@vu.nl>
2025-10-23 23:29:54 +0200

    Added working Rcpp implementation of tuned PGD algorithm.

--- sparse_group_fast.cpp
+++ sparse_group_fast.cpp
@@ -9,6 +9,49 @@ void check(int i){
   Rcpp::Rcout << "Check " << i << std::endl;
 }
 
+arma::uvec change_indices(const arma::vec& x) {
+  
+  // handle edge case
+  if (x.n_elem < 2)
+    return arma::uvec();  // empty
+  
+  // compute differences
+  arma::vec diffx = arma::diff(x);
+  
+  // find where diff != 0
+  arma::uvec change_pos = arma::find(diffx != 0);
+  
+  return change_pos;
+}
+
+// [[Rcpp::export]]
+int rank_Rcpp(arma::mat A){
+  
+  int rank_A = A.n_cols;
+  for(unsigned int j=0; j<A.n_cols; j++){
+    if(arma::all(A.col(j)==0)){
+      rank_A = rank_A - 1;
+    }
+  }
+  return rank_A;
+}
+
+double BIC_Rcpp(const arma::mat& Y,const arma::mat& A,const arma::mat& B){
+  int t = Y.n_rows;
+  arma::mat resid = Y.rows(1,t-1) - Y.rows(0,t-2) - Y.rows(0,t-2)*B*A.t();
+  double SSR = arma::log_det_sympd(resid.t()*resid/t);
+  double penalty = log(t)*(arma::accu(A!=0)+arma::accu(B!=0))/t;
+  return SSR + penalty;
+}
+
+double AIC_Rcpp(const arma::mat& Y,const arma::mat& A,const arma::mat& B){
+  int t = Y.n_rows;
+  arma::mat resid = Y.rows(1,t-1) - Y.rows(0,t-2) - Y.rows(0,t-2)*B*A.t();
+  double SSR = arma::log_det_sympd(resid.t()*resid/t);
+  double penalty = 2*(arma::accu(A!=0)+arma::accu(B!=0))/t;
+  return SSR + penalty;
+}
+
 // [[Rcpp::export]]
 arma::vec softt_vec(arma::vec x,const double lambda){
   unsigned int n = x.n_elem;
@@ -132,7 +175,8 @@ Rcpp::List AB_step_size_Rcpp(const arma::mat& DY,const arma::mat& Y_lag,
 // [[Rcpp::export]]
 Rcpp::List VECM_SG_Rcpp(const arma::mat& A,const arma::mat& B,
                         const arma::mat& Y,const double lambda_L2,
-                        const double lambda_L1=0,const double lambda_R=0,
+                        arma::vec omegas,const double lambda_L1=0,
+                        const double lambda_R=0,
                         double step_init=1,double step_mult=0.5,
                         double step_max_iter=100,
                         int max_iter=1000,double thresh=1e-5,
@@ -146,9 +190,13 @@ Rcpp::List VECM_SG_Rcpp(const arma::mat& A,const arma::mat& B,
   t = t-1;
   arma::mat DYYx2 = 2*DY.t()*Y_lag/t;
   arma::mat YYx2 = 2*Y_lag.t()*Y_lag/t;
-  arma::vec eigval = arma::eig_sym(Y.t()*Y);
-  eigval = arma::reverse(eigval);
-  arma::vec omegas = (t+1)*(t+1)/eigval;
+  
+  //Compute omegas if not user-supplied
+  if (omegas(0) == -999) {
+    arma::vec eigval = arma::eig_sym(Y.t()*Y);
+    eigval = arma::reverse(eigval);
+    omegas = t*t/eigval;
+  }
   
   //Conduct PGD
   arma::vec step_convergence = arma::vec(max_iter);
@@ -159,9 +207,10 @@ Rcpp::List VECM_SG_Rcpp(const arma::mat& A,const arma::mat& B,
   arma::mat AB_new = A*B.t();
   arma::mat AB_old = AB_new;
   arma::mat nabla_A, nabla_B;
-  int iterations, convergence = 0;
-  
+  int iterations = 0;
+  int convergence = 0;
   while(TRUE){
+    Rcpp::checkUserInterrupt();
     
     //Update old coefficients
     A_old = A_new;
@@ -186,7 +235,7 @@ Rcpp::List VECM_SG_Rcpp(const arma::mat& A,const arma::mat& B,
     B_new = Rcpp::as<arma::mat>(AB_step_size_obj["B_new"]);
     AB_new = Rcpp::as<arma::mat>(AB_step_size_obj["AB_new"]);
     step_convergence(iterations) = AB_step_size_obj["step_convergence"];
-
+    
     //Check convergence
     iterations++;
     double AB_dist = std::sqrt(arma::accu(arma::pow(AB_new-AB_old,2)))/n;
@@ -214,5 +263,97 @@ Rcpp::List VECM_SG_Rcpp(const arma::mat& A,const arma::mat& B,
                                          Rcpp::_["step_convergence"]=step_convergence);
   return output;
 }
-                   
-                        
+
+// [[Rcpp::export]]
+Rcpp::List VECM_SG_tuned_Rcpp(const arma::mat& A,const arma::mat& B,
+                              const arma::mat& Y,const arma::mat lambda_grid,
+                              arma::vec omegas,
+                              const double lambda_R=0,
+                              double step_init=1,double step_mult=0.5,
+                              double step_max_iter=100,
+                              int max_iter=1000,double thresh=1e-5,
+                              std::string crit="AIC"){
+  
+  int t = Y.n_rows;
+  int n = Y.n_cols;
+  int n_lambdas = lambda_grid.n_rows;
+  arma::uvec lambda_L2_changes = change_indices(lambda_grid.col(0));
+  double lambda_L1, lambda_L2;
+  if(lambda_grid.n_cols == 1 ){
+    lambda_L1 = 0;
+    }
+  
+  //Compute omegas if not user-supplied
+  if (omegas(0) == -999) {
+    arma::vec eigval = arma::eig_sym(Y.t()*Y);
+    eigval = arma::reverse(eigval);
+    omegas = t*t/eigval;
+  }
+  
+  //Initialize storage objects
+  arma::mat A_old = A;
+  arma::mat B_old = B;
+  arma::cube A_cube = arma::cube(n,n,n_lambdas);
+  arma::cube B_cube = arma::cube(n,n,n_lambdas);
+  arma::vec BICs = arma::vec(n_lambdas);
+  arma::vec AICs = BICs; 
+  arma::vec r_AB = BICs;
+  
+  //Run loop over lambdas
+  for(int j=0; j<n_lambdas; j++){
+    Rcpp::checkUserInterrupt();
+    lambda_L2 = lambda_grid(j,0);
+    if(lambda_grid.n_cols == 2){ 
+      lambda_L1 = lambda_grid(j,1);
+      }
+    Rcpp::List VECM_obj = VECM_SG_Rcpp(A_old,B_old,Y,lambda_L2,omegas,lambda_L1,
+                                       lambda_R,step_init,step_mult,
+                                       step_max_iter,max_iter,thresh,
+                                       false,false);
+    A_cube.slice(j) = Rcpp::as<arma::mat>(VECM_obj["A"]);
+    B_cube.slice(j) = Rcpp::as<arma::mat>(VECM_obj["B"]);
+    
+    //Update initializers for warm start
+    if(arma::any(lambda_L2_changes == j)){
+      int last_lambda_index = arma::min(arma::find(lambda_grid.col(0)==lambda_grid(j-1,0)));
+      A_old = A_cube.slice(last_lambda_index);
+      B_old = B_cube.slice(last_lambda_index);
+      // return Rcpp::List::create(Rcpp::_["lambda_index"] = last_lambda_index,
+      //                           Rcpp::_["lambda_changes"] = lambda_L2_changes,
+      //                           Rcpp::_["j"]=j,
+      //                           Rcpp::_["A_old"]=A_old,
+      //                           Rcpp::_["B_old"]=B_old);
+    }else{
+      A_old = A_cube.slice(j);
+      B_old = B_cube.slice(j);
+    }
+    
+    //Compute BICs
+    r_AB(j) = rank_Rcpp(A_cube.slice(j));
+    BICs(j) = BIC_Rcpp(Y,A_cube.slice(j),B_cube.slice(j));
+    AICs(j) = AIC_Rcpp(Y,A_cube.slice(j),B_cube.slice(j));
+    Rcpp::Rcout << j << std::endl;
+  }
+  
+  //Extract tuned coefficients
+  int ind_opt=0;
+  if(crit=="AIC"){
+    ind_opt = arma::index_min(AICs);
+  }else if(crit=="BIC"){
+    ind_opt = arma::index_min(BICs);
+  }
+  arma::rowvec lambdas_opt = lambda_grid.row(ind_opt);
+  
+  //return results
+  Rcpp::List output = Rcpp::List::create(Rcpp::_["lambda_opt"]=lambdas_opt,
+                                         Rcpp::_["ind_opt"]=ind_opt,
+                                         Rcpp::_["A"] = A_cube.slice(ind_opt),
+                                         Rcpp::_["B"] = B_cube.slice(ind_opt),
+                                         Rcpp::_["r"] = r_AB(ind_opt),
+                                         Rcpp::_["AICs"] = AICs,
+                                         Rcpp::_["BICs"] = BICs,
+                                         Rcpp::_["ranks"] = r_AB,
+                                         Rcpp::_["As"] = A_cube,
+                                         Rcpp::_["Bs"] = B_cube);
+  return output;
+}
